a6e0b86e740e584234ef3c58dadd08b9b51138e1
irotech <irotech@gmail.com>
2015-03-16 23:48:17 +0000

    spring file uploader init

---
+++ Java/Spring/ImageUploader/src/main/java/com/gmail/at/irotech/model/FileUpload.java
@@ -0,0 +1,59 @@
+package com.gmail.at.irotech.model;
+
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import org.springframework.web.multipart.MultipartFile;
+
+@JsonIgnoreProperties({"bytes"})
+public class FileUpload {
+	
+	private MultipartFile file;
+	
+	private String caption;
+	
+	private String alttag;
+	
+	private boolean defaultname;
+	
+	private byte[] bytes;
+
+	public MultipartFile getFile() {
+		return file;
+	}
+
+	public void setFile(MultipartFile file) {
+		this.file = file;
+	}
+
+	public String getCaption() {
+		return caption;
+	}
+
+	public void setCaption(String caption) {
+		this.caption = caption;
+	}
+
+	public String getAlttag() {
+		return alttag;
+	}
+
+	public void setAlttag(String alttag) {
+		this.alttag = alttag;
+	}
+
+	public boolean isDefaultname() {
+		return defaultname;
+	}
+
+	public void setDefaultname(boolean defaultname) {
+		this.defaultname = defaultname;
+	}
+
+	public byte[] getBytes() {
+		return bytes;
+	}
+
+	public void setBytes(byte[] bytes) {
+		this.bytes = bytes;
+	}
+
+}
---
+++ Java/Spring/ImageUploader/src/main/java/com/gmail/at/irotech/model/FileUploadForm.java
@@ -0,0 +1,17 @@
+package com.gmail.at.irotech.model;
+
+import java.util.List;
+
+public class FileUploadForm {
+
+	private List<FileUpload> files;
+
+	public List<FileUpload> getFiles() {
+		return files;
+	}
+
+	public void setFiles(List<FileUpload> files) {
+		this.files = files;
+	}
+	
+}
