036c90dde3784c707c241847abad7c96d8bff188
irotech <irotech@gmail.com>
2015-03-31 00:39:43 +0100

    init spring boot

---
+++ Java/Spring/Spring-boot/simple-spring-boot/src/test/java/com/gmail/at/irotech/simple/SimpleController.java
@@ -0,0 +1,14 @@
+package com.gmail.at.irotech.simple;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+
+  @RequestMapping("/")
+  public String index() {
+    return "Simple Request Mapping...";
+  }
+
+}
