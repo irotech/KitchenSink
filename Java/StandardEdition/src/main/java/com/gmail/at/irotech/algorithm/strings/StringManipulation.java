14b716135f2608aedc3d7049d5c0bd80de049dd8
irotech <irotech@gmail.com>
2017-04-05 21:53:20 +0100

    Some String stuff

---
+++ Java/StandardEdition/src/main/java/com/gmail/at/irotech/algorithm/strings/StringManipulation.java
@@ -0,0 +1,7 @@
+package com.gmail.at.irotech.algorithm.strings;
+
+/**
+ * Created by Ir0 on 30/03/2017.
+ */
+public class StringManipulation {
+}
---
+++ Java/StandardEdition/src/test/java/com/gmail/at/irotech/algorithm/strings/StringManipulationTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by Ir0 on 30/03/2017.
+ */
+public class StringManipulationTest {
+
+}
